f3d52224fd9a8843bbe5f172807515b52d39e3f8
brettmadrid <brettmadrid9@gmail.com>
2020-04-03 17:33:47 -0700

    added pool to knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,14 @@
 module.exports = {
   development: {
     client: "sqlite3",
+    useNullAsDefault: true,
     connection: {
       filename: "./data/lessons.db3"
     },
-    useNullAsDefault: true
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
   }
 };
