465731c28de21002a3c3c6d20bb249bf32288322
brettmadrid <brettmadrid9@gmail.com>
2020-04-12 22:38:32 -0700

    moved back to pre Heroku deployment

--- knexfile.js
+++ knexfile.js
@@ -13,16 +13,4 @@ module.exports = {
       },
     },
   },
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tablename: "knex_migrations",
-      directory: "./migrations",
-    },
-  },
 };
