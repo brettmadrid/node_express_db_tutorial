ba22c467ca3c3e2591b846e2abcf40ed08a45b85
brettmadrid <brettmadrid9@gmail.com>
2020-04-12 20:39:28 -0700

    fixed bug

--- dbConfig.js
+++ dbConfig.js
@@ -1,5 +1,4 @@
-const dbEngine = process.env. || "development";
+const dbEngine = process.env.DB_ENVIRONMENT || "development";
 const config = require("./knexfile")[dbEngine];
 
 module.exports = require("knex")(config);
-
