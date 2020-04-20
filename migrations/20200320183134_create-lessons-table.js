be3620f388f7451df4a7bd3bb5b94f2ee0be0ef4
brettmadrid <brettmadrid9@gmail.com>
2020-04-20 15:35:06 -0700

    reset master

--- migrations/20200320183134_create-lessons-table.js
+++ migrations/20200320183134_create-lessons-table.js
@@ -1,31 +1,22 @@
-exports.up = function(knex) {
-  return knex.schema
-    .createTable("lessons", tbl => {
-      tbl.increments(); // 'id' field
-      tbl.text("name", 128).notNullable();
-      tbl.timestamps(true, true);
-    })
-    .createTable("messages", tbl => {
-      tbl.increments(); // id field
-      tbl
-        .string("sender")
-        .notNullable()
-        .index();
-      tbl.text("text").notNullable();
-      tbl.timestamps(true, true);
+exports.up = function (knex) {
+  return knex.schema.createTable("messages", (tbl) => {
+    tbl.increments(); // id field
+    tbl.string("sender").notNullable().index();
+    tbl.text("text").notNullable();
+    tbl.timestamps(true, true);
 
-      // Foreign key info to 'lessons' table
-      tbl
-        .integer("lesson_id")
-        .unsigned()
-        .notNullable()
-        .references("id")
-        .inTable("lessons")
-        .onDelete("CASCADE")
-        .onUpdate("CASCADE");
-    });
+    // Foreign key info to 'lessons' table
+    tbl
+      .integer("lesson_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("lessons")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTableIfExists("messages").dropTableIfExists("lessons");
 };
