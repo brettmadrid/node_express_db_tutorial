5d51a7c8f0bd10dea6dc2d0c09d0243e36e12fc1
brettmadrid <brettmadrid9@gmail.com>
2020-05-14 18:40:47 -0700

    added helmet, morgan, and cors

--- api/server.js
+++ api/server.js
@@ -1,21 +1,26 @@
-const express = require("express");
-const lessonsRouter = require("../Routes/lessons-routes");
-const messagesRouter = require("../Routes/messages-routes");
-const authRouter = require("../auth/auth-routes");
-const usersRouter = require("../Routes/users-routes");
-const restricted = require("../auth/restricted-middleware");
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+const lessonsRouter = require('../Routes/lessons-routes')
+const messagesRouter = require('../Routes/messages-routes')
+const authRouter = require('../auth/auth-routes')
+const usersRouter = require('../Routes/users-routes')
+const restricted = require('../auth/restricted-middleware')
 
-const server = express();
+const server = express()
+server.use(helmet())
+server.use(morgan('dev'))
+server.use(cors())
+server.use(express.json())
 
-server.use(express.json());
+server.get('/', (req, res) => {
+  res.json({ message: 'I am Son of Hal and am always watching!' })
+})
 
-server.get("/", (req, res) => {
-  res.json({ message: "I am Son of Hal and am always watching!" });
-});
+server.use('/api/auth', authRouter)
+server.use('/api/lessons', restricted, lessonsRouter)
+server.use('/api/messages', restricted, messagesRouter)
+server.use('/api/users', restricted, usersRouter)
 
-server.use("/api/auth", authRouter);
-server.use("/api/lessons", restricted, lessonsRouter);
-server.use("/api/messages", restricted, messagesRouter);
-server.use("/api/users", restricted, usersRouter);
-
-module.exports = server;
+module.exports = server
